50cc3aebbc3976447868d43ab3efa0d1a00a9a7b
TAMARAI SELVAN R <113119433+tamaraiselva@users.noreply.github.com>
2025-02-22 09:13:46 +0530

    Update HomePage.py

--- HomePage.py
+++ HomePage.py
@@ -13,12 +13,6 @@ from PIL import Image
 
 warnings.filterwarnings("ignore")
 
-HUGGINGFACEHUB_API_TOKEN = "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"
-MYSQL_HOST = "localhost"
-MYSQL_USER = "root"
-MYSQL_PASSWORD = "Password"
-MYSQL_DATABASE = "database"
-
 api_key = st.secrets["HUGGINGFACEHUB_API_TOKEN"]
 torch.classes.__path__ = []
 
